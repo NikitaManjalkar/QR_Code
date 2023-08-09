8bea8cbc65b5cd4ad0ebfd85c99507901c1d97f5
NikitaManjalkar <nikitamanjalkar13@gmail.com>
2023-08-09 18:11:34 +0530

    Initial commit

---
+++ src/test/java/Numetry/in/QrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package Numetry.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
